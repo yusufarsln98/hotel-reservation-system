ed084f109e7b88ad64e7808d4fe1f65eceb1871b
Yusuf Arslan <yusufarsln98@outlook.com>
2024-06-12 20:43:47 +0300

    fix: login page

--- src/renderer/pages/Login.tsx
+++ src/renderer/pages/Login.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Flex, Form, Input, Typography } from 'antd';
+import { Button, Flex, Form, Input, Typography, message } from 'antd';
+import { ApiContext } from '../providers/ApiProvider';
 
 export default function Login() {
-  // use form
   const navigate = useNavigate();
+  const { login, isAuth } = useContext(ApiContext);
 
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
-    navigate('/home-page');
+  // check if it is already authenticated
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await isAuth();
+        navigate('/reservations');
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    checkAuth();
+  }, [isAuth, navigate]);
+
+  const onFinish = async (values: any) => {
+    try {
+      await login(values);
+      navigate('/reservations');
+    } catch (err: any) {
+      console.error(err.response.data);
+      message.error(
+        `${err.response.data.code}, ${err.response.data.sqlMessage}`,
+      );
+    }
   };
 
   return (
-    <Flex vertical align="center" gap={20} style={{ marginTop: '20%' }}>
-      <Typography.Title level={2}>Login</Typography.Title>
+    <Flex vertical align="center" gap={20}>
+      <Typography.Title level={2}>Hotel Reservation System</Typography.Title>
+      <Typography.Title level={3}>Login</Typography.Title>
       <Form
         name="basic"
         initialValues={{ remember: true }}
@@ -22,19 +43,16 @@ export default function Login() {
         onFinishFailed={(errorInfo) => {
           console.log('Failed:', errorInfo);
         }}
+        layout="vertical"
       >
         <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
+          label="User"
+          name="user"
+          rules={[{ required: true, message: 'Please input your user!' }]}
         >
-          <Input placeholder="Username" />
+          <Input placeholder="User" />
         </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
+        <Form.Item label="Password" name="password">
           <Input.Password placeholder="Password" />
         </Form.Item>
         <Form.Item>
