a338da82083f99ca3656044cd97a3bfd0fb816c4
Yusuf Arslan <yusufarsln98@outlook.com>
2024-06-16 21:58:46 +0300

    feat: hook for fetching table data

--- src/renderer/hooks/useTableData.ts
+++ src/renderer/hooks/useTableData.ts
@@ -1,5 +1,6 @@
 import { ColumnsType } from 'antd/es/table';
 import { useContext, useEffect, useState } from 'react';
+import { message } from 'antd';
 import { ApiContext } from '../providers/ApiProvider';
 
 const useTableData = (
@@ -14,37 +15,42 @@ const useTableData = (
 
   useEffect(() => {
     const fetchData = async () => {
-      // const tableData = await query(`SELECT * FROM ${tableName}`);
-      const tableData = await query(
-        `SELECT * FROM ${tableName}${where ? ` WHERE ${where}` : ''}`,
-      );
-
-      console.log('tableData', tableData.data);
-
-      // date type is a string in the format 'YYYY-MM-DDTHH:MM:SS.MMMZ'
-      const newData = tableData.data.map((record: any) => {
-        const newRecord = { ...record };
-        Object.keys(newRecord).forEach((key) => {
-          if (typeof newRecord[key] === 'string') {
-            if (newRecord[key].match(/T\d{2}:\d{2}:\d{2}.\d{3}Z/)) {
-              newRecord[key] = new Date(newRecord[key]).toLocaleString();
-            }
-          }
-        });
-        return newRecord;
-      });
-      setData(newData);
-
-      if (newData.length > 0) {
-        setColumns(
-          Object.keys(newData[0]).map((key) => ({
-            title: key,
-            dataIndex: key,
-            key,
-          })),
+      try {
+        // const tableData = await query(`SELECT * FROM ${tableName}`);
+        const tableData = await query(
+          `SELECT * FROM ${tableName}${where ? ` WHERE ${where}` : ''}`,
         );
-      } else {
-        setColumns([]);
+
+        console.log('tableData', tableData.data);
+
+        // date type is a string in the format 'YYYY-MM-DDTHH:MM:SS.MMMZ'
+        const newData = tableData.data.map((record: any) => {
+          const newRecord = { ...record };
+          Object.keys(newRecord).forEach((key) => {
+            if (typeof newRecord[key] === 'string') {
+              if (newRecord[key].match(/T\d{2}:\d{2}:\d{2}.\d{3}Z/)) {
+                newRecord[key] = new Date(newRecord[key]).toLocaleString();
+              }
+            }
+          });
+          return newRecord;
+        });
+        setData(newData);
+
+        if (newData.length > 0) {
+          setColumns(
+            Object.keys(newData[0]).map((key) => ({
+              title: key,
+              dataIndex: key,
+              key,
+            })),
+          );
+        } else {
+          setColumns([]);
+        }
+      } catch (error: any) {
+        console.log('error', error);
+        message.error(JSON.stringify(error.response.data));
       }
     };
     fetchData();
